d396d4d96cc1b359035461eaa5656995afb12299
Aidan Steele <aidan.steele@glassechidna.com.au>
2022-10-14 13:44:43 +1100

    Add sample launch template, allow connectivity over session manager

--- go.mod
+++ go.mod
@@ -6,34 +6,44 @@ require (
 	github.com/aws/aws-sdk-go-v2 v1.16.16
 	github.com/aws/aws-sdk-go-v2/config v1.17.8
 	github.com/aws/aws-sdk-go-v2/service/ec2 v1.63.1
+	github.com/aws/aws-sdk-go-v2/service/ssm v1.31.0
+	github.com/aws/session-manager-plugin v0.0.0-20221012155945-c523002ee02c
+	github.com/davecgh/go-spew v1.1.1
 	github.com/google/gopacket v1.1.19
-	github.com/kor44/extcap v0.0.0-20201215145008-71f6cf07bb46
+	github.com/google/uuid v1.3.0
+	github.com/mmmorris1975/ssm-session-client v0.204.0
+	github.com/spf13/cobra v1.6.0
 	golang.org/x/crypto v0.0.0-20221010152910-d6f0a8c073c2
 	golang.org/x/sync v0.0.0-20220929204114-8fcdb60fdcc0
+	gopkg.in/ini.v1 v1.67.0
 )
 
 require (
+	github.com/aws/aws-sdk-go v1.44.76 // indirect
 	github.com/aws/aws-sdk-go-v2/credentials v1.12.21 // indirect
 	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.12.17 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.23 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.17 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/ini v1.3.24 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.9.17 // indirect
-	github.com/aws/aws-sdk-go-v2/service/ssm v1.30.0 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sso v1.11.23 // indirect
 	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.13.6 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sts v1.16.19 // indirect
 	github.com/aws/smithy-go v1.13.3 // indirect
-	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/cihub/seelog v0.0.0-20170130134532-f561c5e57575 // indirect
+	github.com/fsnotify/fsnotify v1.5.4 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.1 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
-	github.com/russross/blackfriday/v2 v2.1.0 // indirect
-	github.com/spf13/cobra v1.6.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/stretchr/objx v0.4.0 // indirect
 	github.com/stretchr/testify v1.8.0 // indirect
-	github.com/urfave/cli/v2 v2.19.2 // indirect
-	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
-	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
-	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
-	gopkg.in/ini.v1 v1.67.0 // indirect
+	github.com/twinj/uuid v0.0.0-20151029044442-89173bcdda19 // indirect
+	github.com/xtaci/smux v1.5.16 // indirect
+	golang.org/x/net v0.0.0-20220812174116-3211cb980234 // indirect
+	golang.org/x/sys v0.0.0-20220811171246-fbc7d0a398ab // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
+
+replace github.com/mmmorris1975/ssm-session-client => github.com/aidansteele/ssm-session-client v0.0.0-20221014002521-e2f645594da8
--- vpcshark.go
+++ vpcshark.go
@@ -9,11 +9,14 @@ import (
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
+	"github.com/aidansteele/vpcshark/awsdial"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
 	"github.com/aws/aws-sdk-go-v2/service/ec2/types"
 	"github.com/aws/aws-sdk-go-v2/service/ssm"
+	ssmtypes "github.com/aws/aws-sdk-go-v2/service/ssm/types"
+	"github.com/aws/aws-sdk-go/aws/endpoints"
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/layers"
 	"github.com/google/gopacket/pcapgo"
@@ -25,6 +28,7 @@ import (
 	mrand "math/rand"
 	"net"
 	"os"
+	"sort"
 	"strings"
 	"time"
 )
@@ -33,10 +37,11 @@ const trafficMirrorVNI = 0xBEEF
 const trafficMirrorDescription = "vpcshark"
 
 func main() {
+	mrand.Seed(time.Now().UnixNano())
+	fmt.Fprintf(os.Stderr, "%x\n", mrand.Int63())
+
 	j, _ := json.MarshalIndent(os.Args, "", "  ")
 	fmt.Fprintln(os.Stderr, string(j))
-
-	mrand.Seed(time.Now().UnixNano())
 
 	rootCmd := &cobra.Command{RunE: runVpcshark}
 	pf := rootCmd.PersistentFlags()
@@ -52,9 +57,11 @@ func main() {
 	pf.String("extcap-control-out", "", "")
 	pf.String("extcap-control-in", "", "")
 	pf.String("profile", "", "")
+	pf.String("region", "", "")
 	pf.String("vpc", "", "")
 	pf.String("eni", "", "")
 	pf.String("launch-template-id", "", "")
+	pf.String("connectivity", "", "")
 
 	ctx := context.Background()
 	err := rootCmd.ExecuteContext(ctx)
@@ -79,28 +86,31 @@ func runVpcshark(cmd *cobra.Command, args []string) error {
 	extcapControlIn, _ := pf.GetString("extcap-control-in")
 	eni, _ := pf.GetString("eni")
 	profile, _ := pf.GetString("profile")
+	region, _ := pf.GetString("region")
 	vpc, _ := pf.GetString("vpc")
 	launchTemplateId, _ := pf.GetString("launch-template-id")
+	connectivity, _ := pf.GetString("connectivity")
 
 	if cleanup {
 		return runCleanup(ctx, profile)
 	} else if extcapInterfaces {
 		return runExtcapInterfaces(ctx)
 	} else if extcapConfig {
-		return runExtcapConfig(ctx, profile, vpc, extcapReloadOption)
+		return runExtcapConfig(ctx, profile, region, vpc, extcapReloadOption)
 	} else if extcapDLTs {
 		return runExtcapDLTs(ctx)
 	} else if capture {
-		return runCapture(ctx, profile, launchTemplateId, eni, extcapControlIn, extcapControlOut, fifo)
+		return runCapture(ctx, profile, region, launchTemplateId, connectivity, eni, extcapControlIn, extcapControlOut, fifo)
 	} else {
 		return fmt.Errorf("unexpected command")
 	}
 }
 
 type vpcshark struct {
-	gui *Wireshark
-	ec2 *ec2.Client
-	ssm *ssm.Client
+	gui    *Wireshark
+	ec2    *ec2.Client
+	ssm    *ssm.Client
+	region string
 }
 
 func runExtcapInterfaces(ctx context.Context) error {
@@ -120,7 +130,7 @@ func getTag(tags []types.Tag, name string) string {
 	return ""
 }
 
-func runExtcapConfig(ctx context.Context, profile string, vpc string, reloadOption string) error {
+func runExtcapConfig(ctx context.Context, profile, region, vpc, reloadOption string) error {
 	if reloadOption == "vpc" {
 		cfg, err := config.LoadDefaultConfig(ctx, config.WithSharedConfigProfile(profile))
 		if err != nil {
@@ -143,7 +153,7 @@ func runExtcapConfig(ctx context.Context, profile string, vpc string, reloadOpti
 					display = fmt.Sprintf("%s (%s)", vpcId, name)
 				}
 
-				fmt.Printf("value {arg=2}{value=%s}{display=%s}\n", vpcId, display)
+				fmt.Printf("value {arg=3}{value=%s}{display=%s}\n", vpcId, display)
 			}
 		}
 
@@ -168,7 +178,7 @@ func runExtcapConfig(ctx context.Context, profile string, vpc string, reloadOpti
 			for _, template := range page.LaunchTemplates {
 				id := *template.LaunchTemplateId
 				name := *template.LaunchTemplateName
-				fmt.Printf("value {arg=4}{value=%s}{display=%s (%s)}\n", id, id, name)
+				fmt.Printf("value {arg=5}{value=%s}{display=%s (%s)}\n", id, id, name)
 			}
 		}
 
@@ -206,7 +216,7 @@ func runExtcapConfig(ctx context.Context, profile string, vpc string, reloadOpti
 							display = fmt.Sprintf("%s (%s)", display, name)
 						}
 
-						fmt.Printf("value {arg=3}{value=%s}{display=%s}\n", eniId, display)
+						fmt.Printf("value {arg=4}{value=%s}{display=%s}\n", eniId, display)
 					}
 				}
 			}
@@ -215,10 +225,25 @@ func runExtcapConfig(ctx context.Context, profile string, vpc string, reloadOpti
 		return nil
 	}
 
-	fmt.Printf("arg {number=1}{call=--profile}{display=AWS Profile}{type=selector}{required=true}\n")
-	fmt.Printf("arg {number=2}{call=--vpc}{display=VPC}{type=selector}{required=true}{reload=true}{placeholder=Load VPCs...}\n")
-	fmt.Printf("arg {number=3}{call=--eni}{display=Capture ENI}{tooltip=Select ENI(s) to monitor}{type=selector}{reload=true}{placeholder=Load ENIs...}{required=true}\n")
-	fmt.Printf("arg {number=4}{call=--launch-template-id}{display=Mirror target launch template}{tooltip=Select launch template for mirror target}{type=selector}{reload=true}{placeholder=Load templates...}{required=true}\n")
+	fmt.Printf("arg {number=1}{call=--profile}{group=AWS}{display=AWS Profile}{type=selector}{required=true}\n")
+	fmt.Printf("arg {number=2}{call=--region}{group=AWS}{display=AWS Region}{type=selector}{required=true}\n")
+	fmt.Printf("arg {number=3}{call=--vpc}{group=Traffic source}{display=VPC}{type=selector}{required=true}{reload=true}{placeholder=Load VPCs...}\n")
+	fmt.Printf("arg {number=4}{call=--eni}{group=Traffic source}{display=Capture ENI}{tooltip=Select ENI(s) to monitor}{type=selector}{reload=true}{placeholder=Load ENIs...}{required=true}\n")
+	fmt.Printf("arg {number=5}{call=--launch-template-id}{group=Mirror target}{display=Launch template}{tooltip=Select launch template for mirror target}{type=selector}{reload=true}{placeholder=Load templates...}{required=true}\n")
+	fmt.Printf("arg {number=6}{call=--connectivity}{group=Mirror target}{display=Connectivity}{tooltip=Select connectivity to mirror target}{type=selector}{required=true}\n")
+	fmt.Printf("value {arg=6}{value=public-ssh}{display=Via public IP address over SSH}\n")
+	fmt.Printf("value {arg=6}{value=ssm-tunnel}{display=Via port-forwarding over AWS Session Manager}\n")
+
+	regionMap, _ := endpoints.RegionsForService(endpoints.DefaultPartitions(), endpoints.AwsPartitionID, "ec2")
+	regions := []string{}
+	for r := range regionMap {
+		regions = append(regions, r)
+	}
+
+	sort.Strings(regions)
+	for _, region := range regions {
+		fmt.Printf("value {arg=2}{value=%s}{display=%s}\n", region, region)
+	}
 
 	profiles, err := getProfiles(ctx)
 	if err != nil {
@@ -249,6 +274,7 @@ func getProfiles(ctx context.Context) ([]string, error) {
 		}
 	}
 
+	sort.Strings(profiles)
 	return profiles, nil
 }
 
@@ -257,7 +283,7 @@ func runExtcapDLTs(ctx context.Context) error {
 	return nil
 }
 
-func runCapture(ctx context.Context, profile, launchTemplateId, eni, extcapControlIn, extcapControlOut, fifo string) error {
+func runCapture(ctx context.Context, profile, region, launchTemplateId, connectivity, eni, extcapControlIn, extcapControlOut, fifo string) error {
 	fmt.Fprintln(os.Stderr, "starting capture")
 
 	controlOut, err := os.OpenFile(extcapControlOut, os.O_WRONLY, os.ModeNamedPipe)
@@ -284,18 +310,26 @@ func runCapture(ctx context.Context, profile, launchTemplateId, eni, extcapContr
 	}
 	w.log = &wiresharkLog{w: w}
 
-	cfg, err := config.LoadDefaultConfig(ctx, config.WithSharedConfigProfile(profile))
+	fmt.Fprintf(os.Stderr, "loading aws config\n")
+	cfg, err := config.LoadDefaultConfig(
+		ctx,
+		config.WithSharedConfigProfile(profile),
+		config.WithRegion(region),
+		//config.WithClientLogMode(aws.LogRequestWithBody|aws.LogResponseWithBody),
+		config.WithClientLogMode(aws.LogRequest|aws.LogResponse),
+	)
 	if err != nil {
 		return fmt.Errorf(": %w", err)
 	}
 
 	v := &vpcshark{
-		gui: w,
-		ec2: ec2.NewFromConfig(cfg),
-		ssm: ssm.NewFromConfig(cfg),
+		gui:    w,
+		region: region,
+		ec2:    ec2.NewFromConfig(cfg),
+		ssm:    ssm.NewFromConfig(cfg),
 	}
 
-	err = v.ctxmain(ctx, launchTemplateId, eni)
+	err = v.ctxmain(ctx, launchTemplateId, connectivity, eni)
 	if err != nil {
 		_, _ = fmt.Fprintf(v.gui.Log(), "err: %+v\n", err)
 		return err
@@ -358,7 +392,109 @@ func (v *vpcshark) waitForPublicIp(ctx context.Context, instanceId string) (stri
 	return "", fmt.Errorf("couldn't find public ip for instance %s", instanceId)
 }
 
-func (v *vpcshark) ctxmain(ctx context.Context, launchTemplateId, eni string) error {
+func (v *vpcshark) waitForManagedInstance(ctx context.Context, instanceId string) error {
+	// poll until we have a managed instance registered with SSM
+	attempts := 0
+	for attempts < 10 {
+		attempts++
+		v.gui.StatusBar(fmt.Sprintf("Waiting for managed instance %s", instanceId))
+		time.Sleep(5 * time.Second)
+
+		describe, err := v.ssm.DescribeInstanceInformation(ctx, &ssm.DescribeInstanceInformationInput{
+			Filters: []ssmtypes.InstanceInformationStringFilter{
+				{
+					Key:    aws.String("InstanceIds"),
+					Values: []string{instanceId},
+				},
+			},
+		})
+		if err != nil {
+			return fmt.Errorf("listing managed instances: %w", err)
+		}
+
+		if len(describe.InstanceInformationList) == 0 {
+			continue
+		}
+
+		instance := describe.InstanceInformationList[0]
+		if instance.PingStatus == ssmtypes.PingStatusOnline {
+			v.gui.StatusBar(fmt.Sprintf("Got managed instance %s", instanceId))
+			return nil
+		}
+	}
+
+	return fmt.Errorf("couldn't find managed instance %s", instanceId)
+}
+
+func (v *vpcshark) sshClient(ctx context.Context, connectivity string, sshcfg *ssh.ClientConfig, instance types.Instance) (*ssh.Client, error) {
+	instanceId := *instance.InstanceId
+	var dial func(ctx context.Context) (net.Conn, error)
+
+	switch connectivity {
+	case "public-ssh":
+		publicIp, err := v.waitForPublicIp(ctx, *instance.InstanceId)
+		if err != nil {
+			return nil, fmt.Errorf("getting public ip: %w", err)
+		}
+		v.gui.StatusBar(fmt.Sprintf("Trying to establish SSH connectivity to %s", publicIp))
+
+		dial = func(ctx context.Context) (net.Conn, error) {
+			return (&net.Dialer{}).DialContext(ctx, "tcp", publicIp+":22")
+		}
+	case "ssm-tunnel":
+		err := v.waitForManagedInstance(ctx, instanceId)
+		if err != nil {
+			return nil, fmt.Errorf("waiting for managed instance: %w", err)
+		}
+		v.gui.StatusBar(fmt.Sprintf("Trying to establish SSH over SSM connectivity to %s", instanceId))
+
+		dialer := &awsdial.Dialer{Client: v.ssm, Region: v.region}
+		dial = func(ctx context.Context) (net.Conn, error) {
+			return dialer.Dial(ctx, instanceId, 22)
+		}
+	default:
+		return nil, fmt.Errorf("unexpected connectivity option: %s", connectivity)
+	}
+
+	// now we poll until we can connect to the ssh server
+	attempts := 0
+	for attempts < 10 {
+		attempts++
+		fmt.Fprintln(v.gui.Log(), time.Now().String()+" waiting for instance ssh connectivity")
+
+		start := time.Now()
+
+		conn, err := dial(ctx)
+		if err != nil {
+			time.Sleep(sshcfg.Timeout - time.Now().Sub(start))
+			continue
+		}
+
+		_, _, _, err = ssh.NewClientConn(conn, instanceId, sshcfg)
+		if err != nil {
+			conn.Close()
+			time.Sleep(sshcfg.Timeout - time.Now().Sub(start))
+			continue
+		}
+
+		conn.Close()
+		break
+	}
+
+	conn, err := dial(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("establishing port forwarding to ec2 instance: %w", err)
+	}
+
+	cc, chans, reqs, err := ssh.NewClientConn(conn, instanceId, sshcfg)
+	if err != nil {
+		return nil, fmt.Errorf("establishing ssh over port-forwarded ssm tunnel: %w", err)
+	}
+
+	return ssh.NewClient(cc, chans, reqs), nil
+}
+
+func (v *vpcshark) ctxmain(ctx context.Context, launchTemplateId, connectivity, eni string) error {
 	v.gui.StatusBar("starting")
 
 	pub, priv, err := ed25519.GenerateKey(rand.Reader)
@@ -386,11 +522,6 @@ func (v *vpcshark) ctxmain(ctx context.Context, launchTemplateId, eni string) er
 		return err
 	}
 
-	publicIp, err := v.waitForPublicIp(ctx, *instance.InstanceId)
-	if err != nil {
-		return fmt.Errorf("getting public ip: %w", err)
-	}
-
 	signer, err := ssh.NewSignerFromSigner(priv)
 	if err != nil {
 		return fmt.Errorf("getting ssh signer: %w", err)
@@ -406,27 +537,9 @@ func (v *vpcshark) ctxmain(ctx context.Context, launchTemplateId, eni string) er
 		},
 	}
 
-	v.gui.StatusBar(fmt.Sprintf("Trying to establish SSH connectivity to %s", publicIp))
-	// now we poll until we can connect to the ssh server
-	attempts := 0
-	for attempts < 10 {
-		attempts++
-		fmt.Fprintln(v.gui.Log(), time.Now().String()+" waiting for instance ssh connectivity")
-
-		start := time.Now()
-		client, err := ssh.Dial("tcp", publicIp+":22", sshcfg)
-		if err != nil {
-			time.Sleep(sshcfg.Timeout - time.Now().Sub(start))
-			continue
-		}
-
-		client.Close()
-		break
-	}
-
-	client, err := ssh.Dial("tcp", publicIp+":22", sshcfg)
+	client, err := v.sshClient(ctx, connectivity, sshcfg, instance)
 	if err != nil {
-		return fmt.Errorf("dialing ssh server: %w", err)
+		return fmt.Errorf(": %w", err)
 	}
 	defer client.Close()
 
@@ -517,7 +630,7 @@ func (v *vpcshark) createTrafficMirror(ctx context.Context, mirroredEni, capture
 
 	session, err := v.ec2.CreateTrafficMirrorSession(ctx, &ec2.CreateTrafficMirrorSessionInput{
 		NetworkInterfaceId:    &mirroredEni,
-		SessionNumber:         aws.Int32(int32(mrand.Intn(100))),
+		SessionNumber:         aws.Int32(int32(1_000 + mrand.Intn(30_000))),
 		TrafficMirrorFilterId: filterId,
 		TrafficMirrorTargetId: targetId,
 		VirtualNetworkId:      aws.Int32(trafficMirrorVNI),
@@ -649,6 +762,36 @@ func runCleanup(ctx context.Context, profile string) error {
 			Name:   aws.String("description"),
 			Values: []string{trafficMirrorDescription},
 		},
+	}
+
+	ip := ec2.NewDescribeInstancesPaginator(api, &ec2.DescribeInstancesInput{
+		Filters: []types.Filter{
+			{
+				Name:   aws.String("tag-key"),
+				Values: []string{"vpcshark"},
+			},
+			{
+				Name:   aws.String("instance-state-name"),
+				Values: []string{"running"},
+			},
+		},
+	})
+	for ip.HasMorePages() {
+		page, err := ip.NextPage(ctx)
+		if err != nil {
+			return fmt.Errorf(": %w", err)
+		}
+
+		for _, reservation := range page.Reservations {
+			for _, instance := range reservation.Instances {
+				instanceId := instance.InstanceId
+				fmt.Printf("Terminating %s\n", *instanceId)
+				_, err = api.TerminateInstances(ctx, &ec2.TerminateInstancesInput{InstanceIds: []string{*instanceId}})
+				if err != nil {
+					return fmt.Errorf(": %w", err)
+				}
+			}
+		}
 	}
 
 	sp := ec2.NewDescribeTrafficMirrorSessionsPaginator(api, &ec2.DescribeTrafficMirrorSessionsInput{Filters: filters})
